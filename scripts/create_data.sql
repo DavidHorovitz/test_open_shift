373ac5eaef940814b8e354b29191a32ed01cd10e
david horovitz <dovidho@gmail.com>
2025-08-14 14:59:02 +0300

    sql

---
+++ scripts/create_data.sql
@@ -0,0 +1,6 @@
+CREATE TABLE data
+(
+    id int,
+    first_name varchar(50)
+    last_name varchar(50)
+)
---
+++ scripts/insert_data.sql
@@ -0,0 +1,8 @@
+INSERT INTO data
+    (id ,first_name ,last_name)
+VALUES
+  (1,'david','levy'),
+  (2,'ely','gantz'),
+  (3,'ron','dil'),
+  (4,'meny','segal'),
+  (5,'meny','gold')
